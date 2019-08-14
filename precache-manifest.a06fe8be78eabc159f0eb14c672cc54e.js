b76a1a2160aa0c19076fe776aa0ad8e42753dff2
Паирель Дарина Яковлевна <140736@herzen.spb.ru>
2019-08-14 23:05:30 +0300

    Updates

---
+++ precache-manifest.a06fe8be78eabc159f0eb14c672cc54e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182d3ed983a522a745854c8359cfddb9",
+    "url": "/avitotest/index.html"
+  },
+  {
+    "revision": "e50bc83c04236364ba59",
+    "url": "/avitotest/static/css/2.c4c88df4.chunk.css"
+  },
+  {
+    "revision": "5fdb78aafed40d9513da",
+    "url": "/avitotest/static/css/main.27c41f06.chunk.css"
+  },
+  {
+    "revision": "e50bc83c04236364ba59",
+    "url": "/avitotest/static/js/2.78accd47.chunk.js"
+  },
+  {
+    "revision": "5fdb78aafed40d9513da",
+    "url": "/avitotest/static/js/main.5c225535.chunk.js"
+  },
+  {
+    "revision": "4733c91d62305879b026",
+    "url": "/avitotest/static/js/runtime~main.eee80e53.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/avitotest/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/avitotest/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/avitotest/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/avitotest/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/avitotest/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
